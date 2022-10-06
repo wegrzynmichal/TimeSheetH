899440108c3947c3ed106f73662a8f08db0d4af4
Michal Wegrzyn <michal@mr-trade.pl>
2022-10-06 13:18:28 +0200

    Initial commit

---
+++ src/main/java/pl/wegrzynm/TimeSheetH/TimeSheetHApplication.java
@@ -0,0 +1,13 @@
+package pl.wegrzynm.TimeSheetH;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeSheetHApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeSheetHApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/wegrzynm/TimeSheetH/TimeSheetHApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wegrzynm.TimeSheetH;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeSheetHApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
